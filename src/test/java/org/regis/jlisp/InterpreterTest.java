664389486044d4a6dc4d4801df9e6c7d404d2de4
Regis <ruizxu@ebay.com>
2015-03-03 16:25:28 +0800

    Fix parser to handle prefix newline.

--- src/test/java/org/regis/jlisp/InterpreterTest.java
+++ src/test/java/org/regis/jlisp/InterpreterTest.java
@@ -1,5 +1,7 @@
 package org.regis.jlisp;
 
+import java.io.InputStreamReader;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,5 +12,10 @@ public class InterpreterTest {
         Assert.assertEquals(Interpreter.eval("(+ 2 3)"), 5);
         Assert.assertEquals(Interpreter.eval("(+ 2 3)\n"), 5);
         Assert.assertEquals(Interpreter.eval("(+ 2 3)\r"), 5);
+    }
+
+    @Test
+    public void testEval_file() throws Exception {
+        Interpreter.eval(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("test.lisp")));
     }
 }
