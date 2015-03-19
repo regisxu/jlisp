d282fc8769086ef95793837f76fedd2ea872166a
Regis <ruizxu@ebay.com>
2015-03-19 12:54:24 +0800

    Use 'unreflect' to convert Method to MethodHandle.

--- src/main/java/org/regis/jlisp/JInvoker.java
+++ src/main/java/org/regis/jlisp/JInvoker.java
@@ -5,7 +5,6 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.invoke.WrongMethodTypeException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -17,7 +16,7 @@ public class JInvoker {
         Method[] mds = cls.getDeclaredMethods();
         for (Method method : mds) {
             if (method.getName().equals(name)) {
-                mhs.add(toMethodHandle(method));
+                mhs.add(MethodHandles.lookup().unreflect(method));
             }
         }
         if (mhs.size() == 0) {
@@ -25,18 +24,8 @@ public class JInvoker {
         }
     }
 
-    private MethodHandle toMethodHandle(Method method) throws NoSuchMethodException, IllegalAccessException {
-        if (Modifier.isStatic(method.getModifiers())) {
-            return MethodHandles.lookup().findStatic(method.getDeclaringClass(), method.getName(),
-                    MethodType.methodType(method.getReturnType(), method.getParameterTypes()));
-        } else {
-            return MethodHandles.lookup().findVirtual(method.getDeclaringClass(), method.getName(),
-                    MethodType.methodType(method.getReturnType(), method.getParameterTypes()));
-        }
-    }
-
     public Object invoke(Object... args) throws Throwable {
-        Class[] argTypes = new Class[args.length];
+        Class<?>[] argTypes = new Class[args.length];
         for (int i = 0; i < args.length; ++i) {
             argTypes[i] = args[i].getClass();
         }
